619dc5a0a08f0e105b481453a8e2cff3ff495cda
Sachin Hanchate <sachingithub2@gmail.com>
2026-07-23 09:20:48 +0530

    Updated message

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,6 @@
 public class helloworld {
 	public static void main(String[] args) {
 		System.out.println("Hello, World CI with Jenkins!");
+        System.out.println("Hello, World CI with Jenkins and cron jobs!");
 	}
 }
